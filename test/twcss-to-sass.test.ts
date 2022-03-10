11f3662b71b1c8587f5452696d00bf773d2e5e45
Egoist <epsilonelittr@gmail.com>
2022-03-10 17:36:06 +0300

    test: convert comments to class names

--- test/twcss-to-sass.test.ts
+++ test/twcss-to-sass.test.ts
@@ -50,3 +50,38 @@ test('convert to sass with comments', () => {
 
   expect(convertToSass(htmlCotnent)).toBe(sassOutput)
 })
+
+test('convert to sass with comments class names', () => {
+  const htmlCotnent = `<!-- Container Start -->
+<!-- Container Any -->
+<div class="bg-white">
+    <!-- Some Div -->
+    <div class="flex justify-center items-center min-h-screen min-w-full">
+        ...
+    </div>
+</div>
+<!-- Container End-->`
+
+  const sassOutput = `/* Container Start, Container Any -> 1 */
+.pre_container__start__container__any_suf {
+    @apply bg-white;
+
+    /* Some Div -> 2 */
+    .pre_some__div_suf {
+        @apply flex justify-center items-center min-h-screen min-w-full;
+    }
+}`
+
+  const converterConfigs = <any>{
+    useCommentBlocksAsClassName: true,
+    printComments: true,
+    classNameOptions: {
+      lowercase: true,
+      replaceWith: '__',
+      prefix: 'pre_',
+      suffix: '_suf',
+    },
+  }
+
+  expect(convertToSass(htmlCotnent, converterConfigs)).toBe(sassOutput)
+})
